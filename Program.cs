95d792fe533580c6f5d13d21abd1d3bd4924ad24
Anton Olguin <antonolguin1@gmail.com>
2025-02-25 01:57:11 -0600

    WeighedGraph

--- Program.cs
+++ Program.cs
@@ -10,12 +10,44 @@ namespace GraphChiCharp
     {
         static void Main(string[] args)
         {
+            //WeighedGraph graph = new WeighedGraph();
+            //graph.AddVertex("A");
+            //graph.AddVertex("B");
+            //graph.AddVertex("C");
+            //graph.AddVertex("D");
+
+            //graph.AddEdge("A", "B", 1);
+            //graph.AddEdge("A", "C", 4);
+            //graph.AddEdge("B", "C", 2);
+            //graph.AddEdge("B", "D", 5);
+            //graph.AddEdge("C", "D", 1);
+
+            //var distances = graph.Dijkstra("A");
+
+            //foreach (var pair in distances)
+            //{
+            //    Console.WriteLine($"Distancia de A a {pair.Key}: {pair.Value}");
+            //}
+
             Graph graph = new Graph();
+
             graph.AddVertex("A");
             graph.AddVertex("B");
+            graph.AddVertex("C");
 
             graph.AddEdge("A", "B");
+            graph.AddEdge("A", "C");
+            graph.AddEdge("B", "C");
 
+            Console.WriteLine("Grafo inicial:");
+            graph.PrintGraph();
+
+            graph.RemoveEdge("A", "B");
+            Console.WriteLine("\nDespués de eliminar la arista A-B:");
+            graph.PrintGraph();
+
+            graph.RemoveVertex("C");
+            Console.WriteLine("\nDespués de eliminar el vértice C:");
             graph.PrintGraph();
         }
     }
---
+++ WeighedGraph.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GraphChiCharp
+{
+    class WeighedGraph
+    {
+        private Dictionary<string, Dictionary<string, int>> adjList;
+
+        public WeighedGraph()
+        {
+            adjList = new Dictionary<string, Dictionary<string, int>>();
+        }
+
+        public bool AddVertex(string vertex)
+        {
+            if (!adjList.ContainsKey(vertex))
+            {
+                adjList[vertex] = new Dictionary<string, int>();
+                return true;
+            }
+            return false;
+        }
+
+        public bool AddEdge(string vertex1, string vertex2, int weight)
+        {
+            if (adjList.ContainsKey(vertex1) && adjList.ContainsKey(vertex2))
+            {
+                adjList[vertex1][vertex2] = weight;
+                adjList[vertex2][vertex1] = weight;
+                return true;
+            }
+            return false;
+        }
+
+        public Dictionary<string, int> Dijkstra(string startVertex)
+        {
+            var distances = new Dictionary<string, int>();
+            var visited = new HashSet<string>();
+            var previousVertices = new Dictionary<string, string>();
+
+            foreach (var vertex in adjList.Keys)
+            {
+                distances[vertex] = int.MaxValue;
+                previousVertices[vertex] = null;
+            }
+            distances[startVertex] = 0;
+
+            var nodes = new List<string>(adjList.Keys);
+
+            while (nodes.Count > 0)
+            {
+                string currentVertex = null;
+                int smallestDistance = int.MaxValue;
+
+                foreach (var node in nodes)
+                {
+                    if (distances[node] < smallestDistance)
+                    {
+                        smallestDistance = distances[node];
+                        currentVertex = node;
+                    }
+                }
+
+                if (currentVertex == null)
+                    break;
+
+                nodes.Remove(currentVertex);
+                visited.Add(currentVertex);
+
+                foreach (var neighbor in adjList[currentVertex])
+                {
+                    if (visited.Contains(neighbor.Key))
+                        continue;
+
+                    int newDist = distances[currentVertex] + neighbor.Value;
+                    if (newDist < distances[neighbor.Key])
+                    {
+                        distances[neighbor.Key] = newDist;
+                        previousVertices[neighbor.Key] = currentVertex;
+                    }
+                }
+            }
+
+            return distances;
+        }
+
+        public void PrintGraph()
+        {
+            foreach (var pair in adjList)
+            {
+                string vertex = pair.Key;
+                Console.Write(vertex + ": [ ");
+                foreach (var edge in pair.Value)
+                {
+                    Console.Write(edge.Key + "(" + edge.Value + ") ");
+                }
+                Console.WriteLine("]");
+            }
+        }
+    }
+}
